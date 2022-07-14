b392ec0324fcc24fce3b7d272d830a9ef66aeca7
Douglas Oliveira <douglas.toliveira@hotmail.com>
2022-07-14 01:24:29 -0300

    Firts component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButtom from "./components/NotificationButtom"
+
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+     <h1>Hello world!</h1>
+     <NotificationButtom />
+     <NotificationButtom />
+     <NotificationButtom />
+     <NotificationButtom />
+     <NotificationButtom />
+    </>
   )
 }
 
